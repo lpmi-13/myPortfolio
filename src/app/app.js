f3498aabf389e041c26a1f6a70957314f44db249
Will Thirkettle <hello@willthirkettle.co.uk>
2015-12-04 17:25:33 +0000

    Migrating to uiRouter

--- src/app/app.js
+++ src/app/app.js
@@ -2,15 +2,15 @@
 'use strict';
 
 var angular = require('angular');
-var angularRoute = require('angular-route');
+var uiRouter = require('angular-ui-router');
 
 var filters = require('./filters');
 var localisationService = require('./services/localisationService');
 var analyticsService = require('./services/analyticsService');
 
-var viewMain = require('./views/main/main');
 var viewAbout = require('./views/about/about');
-var viewDetail = require('./views/detail/detail');
+var viewProjects = require('./views/projects/projects');
+var viewProject = require('./views/project/project');
 var viewPlay = require('./views/play/play');
 var viewTax = require('./views/tax/tax');
 
@@ -18,29 +18,78 @@ angular.module('myApp', [
 	filters.name,
 	localisationService.name,
 	analyticsService.name,
-	viewMain.name,
 	viewAbout.name,
-	viewDetail.name,
+	viewProjects.name,
+	viewProject.name,
 	viewPlay.name,
 	viewTax.name,
-	'ngRoute'
+	'ui.router'
 ])
+.constant('STATE_URL_HOME_REDIRECT', '/')
+.constant('STATE_URL_ABOUT', '/about')
+.constant('STATE_URL_PROJECTS', '/projects')
+.constant('STATE_URL_PROJECT', '/projects/:projectKey')
+.constant('STATE_URL_PLAY', '/play')
+.constant('STATE_URL_TAX', '/tax')
+.constant('STATE_NAME_HOME_REDIRECT', 'home')
+.constant('STATE_NAME_ABOUT', 'about')
+.constant('STATE_NAME_PROJECTS', 'projects')
+.constant('STATE_NAME_PROJECT', 'projects.project')
+.constant('STATE_NAME_PLAY', 'play')
+.constant('STATE_NAME_TAX', 'tax')
+.constant('DEFAULT_PROJECT_KEY', null)
 .config(function (
-	$routeProvider, $provide
+	$stateProvider,
+	$urlRouterProvider,
+	$provide,
+	STATE_URL_HOME_REDIRECT,
+	STATE_URL_ABOUT,
+	STATE_URL_PROJECTS,
+	STATE_URL_PROJECT,
+	STATE_URL_PLAY,
+	STATE_URL_TAX,
+	STATE_NAME_HOME_REDIRECT,
+	STATE_NAME_ABOUT,
+	STATE_NAME_PROJECTS,
+	STATE_NAME_PROJECT,
+	STATE_NAME_PLAY,
+	STATE_NAME_TAX,
+	DEFAULT_PROJECT_KEY
 ) {
-	// TODO: Migrate to ui:router
-	// handle pages / routing
-	$routeProvider
-		.when('/', { template: '<my:view-main></my:view-main>' })
-		.when('/about', { template: '<my:view-about></my:view-about>' })
-		.when('/project/:key', {
-			template: function (params) {
-				return '<my:view-detail data-key="' + params.key + '"></my:view-detail>';
+	// If 404 - go home...
+	$urlRouterProvider.otherwise(STATE_URL_HOME_REDIRECT);
+
+	// Define the states used in the app
+	$stateProvider
+		.state(STATE_NAME_HOME_REDIRECT, {
+			url: STATE_URL_HOME_REDIRECT,
+			controller: function($state, STATE_NAME_ABOUT) {
+				$state.go(STATE_NAME_ABOUT);
 			}
 		})
-		.when('/play', { template: '<my:view-play></my:view-play>' })
-		.when('/tax', { template: '<my:view-tax></my:view-tax>' })
-		.otherwise({ redirectTo: '/' });
+		.state(STATE_NAME_ABOUT, {
+			url: STATE_URL_ABOUT,
+			template: '<my:view-about></my:view-about>'
+		})
+		.state(STATE_NAME_PROJECTS, {
+			url: STATE_URL_PROJECTS,
+			template: '<my:view-projects></my:view-projects>'
+		})
+		.state(STATE_NAME_PROJECT, {
+			url: STATE_URL_PROJECT,
+			template: '<my:view-project></my:view-project>',
+			params: {
+				projectKey: DEFAULT_PROJECT_KEY
+			}
+		})
+		.state(STATE_NAME_PLAY, {
+			url: STATE_URL_PLAY,
+			template: '<my:view-play></my:view-play>'
+		})
+		.state(STATE_NAME_TAX, {
+			url: STATE_URL_TAX,
+			template: '<my:view-tax></my:view-tax>'
+		});
 
 	// decorate the $q service with 'allSettled' which unlike 'all' resolves if a promise fails
 	$provide.decorator('$q', function($delegate) {
